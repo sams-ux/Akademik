1d025467c2a4908145a7d8263848d381d03400ee
sams-ux <msamsudin423@gmail.com>
2025-07-09 10:54:08 +0700

    Pertemuan 13

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const mahasiswaRoutes = require('./routes/mahasiswa');
+const matakuliahRoutes = require('./routes/matakuliah');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/mahasiswa', mahasiswaRoutes);
+app.use('/matakuliah', matakuliahRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
